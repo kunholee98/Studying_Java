75021f9cd6f8edd28062ef03effd4a084cb343d6
kunholee_98 <dlrjsgh750@naver.com>
2021-11-22 20:58:42 +0900

    패키지 생성 및 성적 관리 시스템

---
+++ chap01/chap01/test.java
@@ -0,0 +1,11 @@
+package chap01;
+
+public class test {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        for (int num : arr){
+            System.out.println(num);
+        }
+    }
+}
+
