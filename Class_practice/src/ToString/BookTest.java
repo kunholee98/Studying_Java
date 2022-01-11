463fe67a6bd49951c4f507bf128dcac5f182ae04
kunholee_98 <dlrjsgh750@naver.com>
2022-01-11 14:52:13 +0900

    toString override practice

---
+++ Class_practice/src/ToString/BookTest.java
@@ -0,0 +1,24 @@
+package ToString;
+
+class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return title + "의 작가는 " + author + "입니다.";
+    }
+}
+
+public class BookTest {
+    public static void main(String[] args) {
+        Book book = new Book("데미안", "헤르만 헤세");
+
+        System.out.println(book);
+    }
+}
